5d1bdd56f33c23de8bbb45e614cf67bcc5fa8c84
Nav-ee-n <naveenfelix876@gmail.com>
2022-07-06 15:22:43 +0530

    Created CompanyName

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompanyNameComponent } from './company-name/company-name.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyNameComponent
   ],
   imports: [
     BrowserModule,
