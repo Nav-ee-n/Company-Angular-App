670512c9396c89d9f69461329b2c4b1f5241496b
Nav-ee-n <naveenfelix876@gmail.com>
2022-07-06 15:44:45 +0530

    Created ManagerName

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CompanyNameComponent } from './company-name/company-name.component';
+import { ManagerNameComponent } from './manager-name/manager-name.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CompanyNameComponent
+    CompanyNameComponent,
+    ManagerNameComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/manager-name/manager-name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manager-name',
+  templateUrl: './manager-name.component.html',
+  styleUrls: ['./manager-name.component.css']
+})
+export class ManagerNameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
